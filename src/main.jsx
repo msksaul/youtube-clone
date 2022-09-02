20754d5d924da091ad8d8b986181455bdf8cbd05
mscalel <msksaul@gmail.com>
2022-09-02 15:14:15 -0600

    Cleaning Project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+import App from './App';
+import './main.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<App />)
